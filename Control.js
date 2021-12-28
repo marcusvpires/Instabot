b4cb7cc7bc605ff377cd01cc7ba5d760406d549c
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2021-12-28 19:06:18 -0300

    Console log

--- Control.js
+++ Control.js
@@ -2,12 +2,11 @@ const puppeteer = require("puppeteer");
 const fs = require("fs").promises;
 
 async function controller() {
+  console.log('= Start intabot')
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await login(page);
-
   await delay(2);
-
   await scrapProfileLinks(page);
   // await browser.close();
   console.log("-- Script end");
@@ -21,22 +20,26 @@ async function controller() {
 
 async function login(page) {
   try {
-    // Check previous cookies
+    console.log("- Login")
+    console.log("  Check previous cookies")
     const cookiesString = await fs.readFile("./Storage/cookies.json");
     const cookies = JSON.parse(cookiesString);
     await page.setCookie(...cookies);
     await page.goto("https://www.instagram.com/");
+    console.log("  Cookies restored")
   } catch (err) {
-    console.log("Can't find cookies, start login...");
+    console.log("  Cookies not found");
     await setCookies(page);
   }
   await page.waitForSelector("button");
   await delay(2);
+  console.log('  Close notification popup')
   await buttonContain(page, "Agora não");
-  console.info("-- Login");
+  console.info("- Login complete");
 }
 
 async function setCookies(page) {
+  console.log('  Set new login')
   await page.goto("https://www.instagram.com/accounts/login/");
   await page.waitForSelector('input[name="username"]');
   await page.type('input[name="username"]', "lxl3ehl53");
@@ -45,9 +48,10 @@ async function setCookies(page) {
   await page.waitForNavigation();
   await page.waitForSelector("button");
   await delay(2);
+  console.log('  Close notification popup')
   await buttonContain(page, "Agora não");
   await page.waitForNavigation();
-  // Save cookies in ./Storage/cookies.json
+  console.log('  Save cookies in (./Storage/cookies.json)')
   const cookies = await page.cookies();
   await fs.writeFile(
     "./Storage/cookies.json",
@@ -58,12 +62,14 @@ async function setCookies(page) {
 /* --------------------------- Scrap profile links -------------------------- */
 
 async function scrapProfileLinks(page) {
+  console.log('- Scrap profile links')
   await openLikedByList(page);
   await delay(5)
   await scrapNames(page)
 }
 
 async function openLikedByList(page) {
+  console.log('  Open Linked_BY list')
   await page.evaluate(() => {
     function parentButton(element, maxParentsPassed = 10) {
       for (let ix = 0; ix < maxParentsPassed; ix++) {
@@ -92,14 +98,14 @@ async function openLikedByList(page) {
 }
 
 async function scrapNames(page) {
+  console.log('- Start names scrap')
   let names = []
   let counter = 0
   while (true) {
     if (counter > 10) {
-      console.log(`-- Scrap loop limit <Total ${names.length}>`)
+      console.log('  Scrap loop limit <Total', names.length + '>')
       break
     }
-    console.log('   Counter:', counter)
     const profileNames = await page.evaluate(() => {
       const likedBySection = document.querySelector('div[aria-label="Curtidas"]');
       const profiles = likedBySection.querySelectorAll("span > a");
@@ -113,10 +119,10 @@ async function scrapNames(page) {
     });
     names.push(...profileNames)
     if (profileNames.length > 0) {
-      console.log(`-- Scrap ${profileNames.length} new profiles <Total ${names.length}>`)
+      console.log('  Scrap:', profileNames.length, '|', names.length)
     }
     else {
-      console.log(`-- Scrap all profiles <Total ${names.length}>`)
+      console.log('= Scrap all <Total', names.length + '>')
       break;
     }
     counter++
@@ -129,7 +135,7 @@ async function scrapNames(page) {
 /* -------------------------------------------------------------------------- */
 
 function delay(time) {
-  console.log('   delay:', (Number(time) * 1000) + 'ms')
+  console.log('  oo', (Number(time) * 1000) + 'ms')
   return new Promise(function (resolve) {
     setTimeout(resolve, (Number(time) * 1000));
   });
