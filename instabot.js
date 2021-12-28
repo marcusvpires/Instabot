b838d0dd9555c34f220332f7092205a3aab81dbb
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2021-12-28 03:04:11 -0300

    Reuse cookies

--- instabot.js
+++ instabot.js
@@ -1,9 +1,41 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs").promises;
 
-(async function login(page) {
+async function removeSaveInfoPopup(page) {
+  const [button] = await page.$x("//button[contains(., 'Agora não')]");
+  if (button) {
+    await button.click();
+  }
+}
+
+async function login(page) {
   await page.goto("https://www.instagram.com/accounts/login/");
   await page.waitForSelector('input[name="username"]');
   await page.type('input[name="username"]', "czdvbu3ac");
   await page.type('input[name="password"]', "asdf9876");
   await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+  await page.waitForSelector("button");
+
+  const cookies = await page.cookies();
+  await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
+}
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  try {
+    const cookiesString = await fs.readFile("./cookies.json");
+    const cookies = JSON.parse(cookiesString);
+    await page.setCookie(...cookies);
+    await page.goto("https://www.instagram.com/");
+  } catch (err) {
+    console.log("Can't find cookies, start login...");
+    login(page);
+    await page.waitForNavigation();
+
+    await delay(8000);
+  }
+
 })();
