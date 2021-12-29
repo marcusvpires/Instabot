e9b704b7323fe0e9f335f51f2724994250960ae8
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2021-12-29 15:45:12 -0300

    Refactor code

--- Control.js
+++ Control.js
@@ -1,62 +1,72 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs").promises;
 
-async function controller() {
-  console.log("= Start intabot");
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await login(page);
-  await delay(2);
-  /*   
-  for (let ix = 0; ix < 10; ix++) {
-    await scrapProfileLinks(page);
-    await delay(5)
-    console.log("\n= Restart scrap profile links");
-  } 
-  */
-  await likeProfiles(page);
-  // await browser.close();
-  console.log("\n= Script end");
+/* -------------------------------------------------------------------------- */
+/*                                  Variables                                 */
+/* -------------------------------------------------------------------------- */
+
+const userName = "lxl3ehl53";
+const password = "asdf9876";
+const minProfiles = 150;
+const maxLikes = 150
+let likeCounter = 0
+
+/* -------------------------------------------------------------------------- */
+/*                                   Control                                  */
+/* -------------------------------------------------------------------------- */
+
+async function control() {
+  try {
+    log.header();
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await openInstagram(page);
+    await scrapProfiles(page);
+    await likeProfiles(page);
+  } catch (err) {
+    console.group('\x1b[31m' + err.message + '\x1b[0m')
+    console.log(err)
+    console.groupEnd()
+  }
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                   Actions                                  */
 /* -------------------------------------------------------------------------- */
 
-/* ---------------------------------- Login --------------------------------- */
+/* ----------------------------- Open Instagram ----------------------------- */
 
-async function login(page) {
+async function openInstagram(page) {
+  log.title("Open instagram");
   try {
-    console.log("\n- Login");
-    console.log("  Check previous cookies");
+    log.info("Check cookies saved in <cookies.json>");
     const cookiesString = await fs.readFile("./Storage/cookies.json");
     const cookies = JSON.parse(cookiesString);
     await page.setCookie(...cookies);
     await page.goto("https://www.instagram.com/");
-    console.log("  Cookies restored");
+    log.info("Cookies reset in this session");
   } catch (err) {
-    console.log("  Cookies not found");
-    await setCookies(page);
+    log.info("Cookies not found");
+    await login(page);
   }
   await page.waitForSelector("button");
   await delay(2);
-  console.log("  Close notification popup");
   await buttonContain(page, "Agora não");
-  console.info("\n- Login complete");
+  log.info("Close notification popup");
 }
 
-async function setCookies(page) {
-  console.log("  Set new login");
+async function login(page) {
+  log.info(`Starting login --user: <${userName}>`);
   await page.goto("https://www.instagram.com/accounts/login/");
   await page.waitForSelector('input[name="username"]');
-  await page.type('input[name="username"]', "lxl3ehl53");
-  await page.type('input[name="password"]', "asdf9876");
+  await page.type('input[name="username"]', userName);
+  await page.type('input[name="password"]', password);
   await page.click('button[type="submit"]');
   await page.waitForNavigation();
   await page.waitForSelector("button");
-  await delay(2);
-  console.log("  Close notification popup");
+  await delay(1);
   await buttonContain(page, "Agora não");
+  log.info("Close notification popup");
   await page.waitForNavigation();
   console.log("  Save cookies in (./Storage/cookies.json)");
   const cookies = await page.cookies();
@@ -64,130 +74,137 @@ async function setCookies(page) {
     "./Storage/cookies.json",
     JSON.stringify(cookies, null, 2)
   );
+  log.info("Save cookies saved in <cookies.json>");
 }
 
-/* --------------------------- Scrap profile links -------------------------- */
+/* ----------------------------- Scrap profiles ----------------------------- */
 
-async function scrapProfileLinks(page) {
-  console.log("\n- Scrap profile links");
-  await openLikedByList(page);
-  await delay(5);
-  const profileNames = await scrapNames(page);
-  console.log("\n- Storage names in (./Storage/profiles.json)");
+async function scrapProfiles(page) {
+  let profiles = [];
+  if (minProfiles <= 0) return [];
+  for (let index = 0; index < 10; index++) {
+    profiles.push(...(await scrapArticleProfiles(page)));
+    log.info(`Total: ${profiles.length}`);
+    if (profiles.length < minProfiles) {
+      log.info(`Restart scrap profiles`);
+    } else break;
+  }
   await fs.writeFile(
     "./Storage/profiles.json",
-    JSON.stringify(profileNames, null, 2)
+    JSON.stringify(profiles, null, 2)
   );
+  log.info(`Store profile names in <profiles.json>`);
 }
 
-async function openLikedByList(page) {
-  console.log("  Open Linked_BY list");
-  await page.evaluate(() => {
-    function parentButton(element, maxParentsPassed = 10) {
-      for (let ix = 0; ix < maxParentsPassed; ix++) {
-        element = element.parentElement;
-        if (element.tagName === "BUTTON") {
-          return element;
-        }
-      }
+async function scrapArticleProfiles(page) {
+  log.title("Scrap profile names");
+  await page.evaluate(openProfileList);
+  log.info("Open list of profiles that like the post");
+  await delay(5);
+  const profileNames = await scrapNames(page);
+  return profileNames;
+}
+
+async function openProfileList() {
+  const article = document.querySelector("article");
+  article.scrollIntoView({ block: "center", inline: "nearest" });
+  let button = article.querySelector('svg[aria-label="Curtir"][width="24"]');
+  if (button) {
+    for (let index = 0; index < 10; index++) {
+      button = button.parentElement;
+      if (button.tagName === "BUTTON") break;
     }
-    const article = document.querySelector("article");
-    article.scrollIntoView({ block: "center", inline: "nearest" });
-    const flag = article.querySelector('svg[aria-label="Curtir"][width="24"]');
-    // Mark article with like
-    if (flag) {
-      let likeButton = parentButton(flag);
-      likeButton.click();
-    }
-    // Open likers list
-    let likeProfileList = article.querySelector('a[href*="liked_by"]');
-    if (likeProfileList) {
-      likeProfileList.click();
-    }
-    // Remove article from this section
-    article.remove();
-  });
+    button.click();
+  }
+  const listButton = article.querySelector('a[href*="liked_by"]');
+  if (listButton) listButton.click();
+  article.remove();
 }
 
 async function scrapNames(page) {
-  console.log("\n- Start names scrap");
-  let names = [];
-  let counter = 0;
-  while (true) {
-    if (counter > 10) {
-      console.log("  Scrap loop limit <Total", names.length + ">");
-      break;
-    }
-    const profileNames = await page.evaluate(() => {
-      const likedBySection = document.querySelector(
-        'div[aria-label="Curtidas"]'
-      );
-      if (!likedBySection) return [];
-      const profiles = likedBySection.querySelectorAll("span > a");
-      const profileNames = Object.values(profiles).map((element) => {
-        const name = element.attributes.title.value;
-        element.scrollIntoView({ block: "center", inline: "nearest" });
-        element.remove();
-        return name;
-      });
-      return profileNames;
-    });
-    names.push(...profileNames);
-    if (profileNames.length > 0) {
-      console.log("  Scrap:", profileNames.length, "|", names.length);
-    } else {
-      console.log("\n= Scrap all <Total", names.length + ">");
-      break;
-    }
-    counter++;
+  let profiles = [];
+  for (let index = 0; index < 15; index++) {
+    const sectionProfiles = await page.evaluate(scrapSectionNames);
+    profiles.push(...sectionProfiles);
+    log.info(
+      `Section: ${profiles.length} -- ${sectionProfiles.length} new saved profile names`
+    );
+    if (sectionProfiles.length <= 0) break;
     await delay(4);
   }
-  return names;
+  return profiles;
+}
+
+function scrapSectionNames() {
+  const likedBySection = document.querySelector('div[aria-label="Curtidas"]');
+  if (!likedBySection) return [];
+  const profiles = likedBySection.querySelectorAll("span > a");
+  return Object.values(profiles).map((element) => {
+    const name = element.attributes.title.value;
+    element.scrollIntoView({ block: "center", inline: "nearest" });
+    element.remove();
+    return name;
+  });
 }
 
 /* ------------------------------ Like profiles ----------------------------- */
 
 async function likeProfiles(page) {
-  console.log("\n- Like profiles");
-  const profileListString = await fs.readFile("./Storage/profiles.json");
-  const profileList = JSON.parse(profileListString);
-  for (var ix = 0; ix < profileList.length; ix++) {
-    console.log("  Open", profileList[ix]);
-    await page.goto("https://www.instagram.com/" + profileList[ix]);
+  let profiles = await fs.readFile("./Storage/profiles.json");
+  profiles = JSON.parse(profiles);
+  for (var index = 0; index < profiles.length; index++) {
+    log.title(`Like profile ${index} <${profiles[index]}>`);
+    await page.goto("https://www.instagram.com/" + profiles[index]);
     await delay(5);
-    await postLikes(page);
+    await likeProfile(page);
+    if (likeCounter >= maxLikes) {
+      console.title(`Maximum number of likes reached <${likeCounter}/${maxLikes}>`)
+      return
+    }
   }
 }
 
-async function postLikes(page) {
-  const links = await page.evaluate(() => {
-    let followers = document.querySelector('a[href*="followers"] > span');
-    if (followers && Number(followers.title) < 300) return [];
+async function likeProfile(page) {
+  const [posts, followers] = await page.evaluate(scrapPosts);
+  log.info(`Number of profile followers: ${followers}`);
+  log.info(`Number of profile photo links collected: ${posts.length}`);
+  if (followers && Number(followers.title) < 300) return;
+  for (var index = 0; index < posts.length; index++) {
+    await page.goto(posts[index]);
+    await delay(4);
+    let liked = await page.evaluate(likePost);
+    if (liked) {
+      likeCounter++; 
+      log.info(`C:${likeCounter} - Post liked: ${posts[index]}`);
+      if (likeCounter >= maxLikes) return
+    }
+    await delay(4);
+  }
+}
+
+function likePost() {
+  let button = document.querySelector(
+    'svg[aria-label="Curtir"][width="24"]'
+  );
+  if (!button) return;
+  for (let index = 0; index < 10; index++) {
+    button = button.parentElement;
+    if (button.tagName === "BUTTON") break;
+  }
+  button.click();
+  return true
+}
+
+function scrapPosts() {
+  try {
+    let followers = document.querySelector('a[href*="followers"] > span').title;
     const article = document.querySelector("article > div");
-    const htmlLinks = article.querySelectorAll("a");
-    const links = Object.values(htmlLinks).map((link) => link.href);
-    if (links.length > 7) links.length = 7;
-    return links;
-  });
-  console.log(links);
-  for (var ix = 0; ix < links.length; ix++) {
-    console.log("  - post", links[ix]);
-    await page.goto(links[ix]);
-    await delay(4);
-    await page.evaluate(() => {
-      let likeButton = document.querySelector(
-        'svg[aria-label="Curtir"][width="24"]'
-      );
-      if (!likeButton) return;
-      for (let ix = 0; ix < 10; ix++) {
-        likeButton = likeButton.parentElement;
-        if (likeButton.tagName === "BUTTON") {
-          break;
-        }
-      }
-      likeButton.click();
-    });
-    await delay(4);
+    let posts = article.querySelectorAll("a");
+    posts = Object.values(posts).map((post) => post.href);
+    if (posts.length > 7) posts.length = 7;
+    return [posts, followers];
+  } catch (err) {
+    return [[], 0]
   }
 }
 
@@ -196,7 +213,6 @@ async function postLikes(page) {
 /* -------------------------------------------------------------------------- */
 
 function delay(time) {
-  console.log("  oo", Number(time) * 1000 + "ms");
   return new Promise(function (resolve) {
     setTimeout(resolve, Number(time) * 1000);
   });
@@ -211,4 +227,21 @@ async function buttonContain(page, innerText) {
   }
 }
 
-controller();
+const log = {
+  header: () => {
+    console.clear();
+    console.log(`
+|============================================================|
+|                          Instabot                          |
+|============================================================|
+    `);
+  },
+  title: (txt) => {
+    console.log("\n\x1b[32m| " + txt + "\x1b[0m");
+  },
+  info: (txt) => {
+    console.log("| " + txt);
+  },
+};
+
+control(5)
